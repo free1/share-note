85753e03330ef7734b0ca6559d132eecc6fefba7
free <747549945@qq.com>
2013-07-06 21:33:32 +0800

    UserInfo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 #encoding: utf-8
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
   	@user = User.new
   end
@@ -14,4 +19,5 @@ class UsersController < ApplicationController
   		render 'new'	
   	end
   end
+
 end
