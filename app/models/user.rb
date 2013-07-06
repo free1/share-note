901cfb65f1411b041050ce790cfad7474d90d2a0
free <747549945@qq.com>
2013-07-06 17:13:41 +0800

    text

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   before_save { |user| user.email = email.downcase }
-  before_save :create_remember_token
+  before_create { create_remember_token(:remember_token) }
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -14,18 +14,14 @@ class User < ActiveRecord::Base
   validates :password_confirmation, presence: true
 
   def send_password_reset
-    generate_token(:password_reset_token)
+    create_remember_token(:password_reset_token)
     self.password_reset_sent_at = Time.zone.now
     save!
     UserMailer.password_reset(self).deliver
   end
 
   private
-    def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
-    end
-
-    def generate_token(column)
+    def create_remember_token(column)
       begin
         self[column] = SecureRandom.urlsafe_base64
       end while User.exists?(column => self[column])
