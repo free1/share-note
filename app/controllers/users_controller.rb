c7838fc5ea264cabec20cb3de5186dfbebb45f63
free <747549945@qq.com>
2013-07-09 11:00:14 +0800

    updateValidate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,10 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    # if params[:user][:password].blank?
+    #   params[:user].delete(:password)
+    #   params[:user].delete(:password_confirmation)
+    # end
     if @user.update_attributes(params[:user])
       flash[:success] = "更新资料成功!"
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation,  :website, :github,
                   :twitter, :qq, :city, :company, :position, :autograph, :resume
@@ -6,15 +7,23 @@ class User < ActiveRecord::Base
   before_save { |user| user.email = email.downcase }
   before_create { create_remember_token(:remember_token) }
 
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true,
-                    format: { with: VALID_EMAIL_REGEX },
-                    uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }, :if => :new_record?
-  validates :password_confirmation, presence: true, :if => :new_record?
+  # validates :name, presence: true, length: { maximum: 50 }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  # validates :email, presence: true,
+  #                   format: { with: VALID_EMAIL_REGEX },
+  #                   uniqueness: { case_sensitive: false }
+  # validates :password, presence: true, length: { minimum: 6 }, :if => :new_record?
+  # validates :password_confirmation, presence: true, :if => :new_record?
 
-  
+  validates_presence_of :name, :email, :password, :password_confirmation, 
+                        message: "不能为空!", if: :new_record? #password reset bug
+  validates_length_of :name, :password, in: 1..20, message: "必须为1到10的字符!",
+                        if: :new_record? #password reset bug
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX, message: "格式不正确!"
+  validates_uniqueness_of :email, case_sensitive: false, message: "已存在!"
+
+  validates_numericality_of :qq, only_integer: true, allow_nil: true, message: "必须是整数!"
 
   def send_password_reset
     create_remember_token(:password_reset_token)
