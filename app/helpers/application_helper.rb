94c099682c9cb96160ba38c7a5e84e70775f6c73
free <747549945@qq.com>
2013-08-07 21:54:45 +0800

    增加文章显示

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,18 @@ module ApplicationHelper
 		 	CodeRay.scan(code, language).div(:tab_width=>2)
 		end
 	end
+
+	# 使冲破布局的内容自动换行
+	def wrap(content)
+		sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+	end
+
+	private
+
+		def wrap_long_string(text, max_width = 95)
+		  zero_width_space = "&#8203;"
+		  regex = /.{1,#{max_width}}/
+		  (text.length < max_width) ? text :
+		                              text.scan(regex).join(zero_width_space)
+		end
 end
